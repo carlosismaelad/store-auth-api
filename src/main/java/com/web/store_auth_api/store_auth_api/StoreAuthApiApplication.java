17051d5d31512c6b40d5fbb28e1442a4a8f34540
Carlos Dourado <carlosismaelad@outlook.com>
2024-10-20 11:44:53 -0300

    Initial commit

---
+++ src/main/java/com/web/store_auth_api/store_auth_api/StoreAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.web.store_auth_api.store_auth_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreAuthApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreAuthApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/store_auth_api/store_auth_api/StoreAuthApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.store_auth_api.store_auth_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreAuthApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
